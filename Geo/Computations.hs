857eff0ce697b1c563cd81f18ea7fb584f0a679d
Thomas M. DuBuisson <thomas.dubuisson@gmail.com>
2012-08-06 22:15:25 -0700

    Missing files

---
+++ Geo/Computations.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances, EmptyDataDecls, BangPatterns, TupleSections #-}
+-- |A basic GPS library with calculations for distance and speed along
+-- with helper functions for filtering/smoothing trails.  All distances
+-- are in meters and time is in seconds.  Speed is thus meters/second
+
+module Geo.Computations
+       ( module Geo.Computations.Basic
+       , module Geo.Computations.Trail
+       ) where
+
+import Geo.Computations.Basic
+import Geo.Computations.Trail
